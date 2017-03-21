2a43e9da92667b30e48191b629d1d65200770d10
Viktor Kemeny <kemeny.viktor@hotmail.com>
2017-03-21 16:14:21 +0100

    More exercises uploaded

---
+++ Week02/Day02/src/Exercise12.java
@@ -0,0 +1,22 @@
+/**
+ * Created by pc on 2017.03.21..
+ */
+public class Exercise12 {
+
+  public static void main(String[] args) {
+    int a = 123;
+    int b = 526;
+    int temp = a;
+    a = b;
+    b = temp;
+    // Swap the values of the wariables
+
+
+
+
+    System.out.println(a);
+    System.out.println(b);
+
+  }
+
+}
