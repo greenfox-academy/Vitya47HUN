bd2e1453410fdaec46d7e86dbe5125641f98aa14
Viktor Kemeny <kemeny.viktor@hotmail.com>
2017-03-22 14:54:38 +0100

    Added day 03 exercises

---
+++ Week02/Day03/src/Exercise001.java
@@ -0,0 +1,16 @@
+public class Exercise001 {
+  public static void main(String[] args) {
+// - Create an integer variable named `ak` and assign the value `123` to it
+// - Create a function called `doubling` that doubles it's input parameter
+// - Print the result of `doubling(ak)`
+
+    int ak = 123;
+      System.out.println(doubling(ak));
+  }
+  public static int doubling(int ak) {
+
+    return ak * 2;
+
+  }
+
+}
---
+++ Week02/Day03/src/Exercise002.java
@@ -0,0 +1,5 @@
+/**
+ * Created by pc on 2017.03.22..
+ */
+public class Exercise002 {
+}
