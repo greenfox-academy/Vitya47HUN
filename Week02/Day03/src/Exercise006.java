b998a687a6badcb9bfd7c5d3caa347a9cbeab52b
Viktor Kemeny <kemeny.viktor@hotmail.com>
2017-03-22 16:42:37 +0100

    Added Exercise006

--- Week02/Day03/src/Exercise006.java
+++ Week02/Day03/src/Exercise006.java
@@ -5,4 +5,17 @@
 //   which prints the input String parameters
 //   (can have multiple number of arguments)
 public class Exercise006 {
+  public static void main(String[] args) {
+    String [] texts = {"a","b","c","d"};
+
+    printer(texts);
+  }
+
+  public static void printer(String... texts) {
+    for (String random:texts) {
+      System.out.println(random);
+
+    }
+
+  }
 }
