31d1baa43f5112f659dc1d0912fc42477c58a120
Viktor Kemeny <kemeny.viktor@hotmail.com>
2017-03-22 12:09:12 +0100

    Added more Day03 exercises

---
+++ Week02/Day03/src/Exercise23.java
@@ -0,0 +1,14 @@
+
+public class Exercise23 {
+  public static void main(String[] args) {
+    // Create a program that writes this line 100 times:
+// "I won't cheat on the exam!"
+    String a = "I won't cheat on the exam!";
+    int count = 0;
+    while (count < 100){
+      System.out.println(a);
+      count++;
+    }
+
+  }
+}
---
+++ Week02/Day03/src/Exercise24.java
@@ -0,0 +1,12 @@
+// Create a program that prints all the even numbers between 0 and 500
+public class Exercise24 {
+  public static void main(String[] args) {
+
+
+    for (int i = 0; i < 500; i++) {
+      if (i % 2 == 0)
+        System.out.println(i);
+
+    }
+  }
+}
