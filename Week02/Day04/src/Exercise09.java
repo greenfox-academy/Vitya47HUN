c359678b5ee32518201fedb58b3eece524a8e5a1
Viktor Kemeny <kemeny.viktor@hotmail.com>
2017-03-23 17:30:09 +0100

    Exercise09(Finished exercise09 with for loop)

--- Week02/Day04/src/Exercise09.java
+++ Week02/Day04/src/Exercise09.java
@@ -1,6 +1,7 @@
 import java.util.*;
-public class Exercise09{
-  public static void main(String... args){
+
+public class Exercise09 {
+  public static void main(String... args) {
     ArrayList<Object> arrayList = new ArrayList<Object>();
     arrayList.add("Cupcake");
     arrayList.add(2);
@@ -11,10 +12,14 @@ public class Exercise09{
     // Your task is to change from "2" to "Croissant" and change from "false" to "Ice cream"
     // No, don't just remove the lines
 
-    
+    for (int i = 0; i <= 4; i++) {
+      if (i == arrayList.indexOf(2)) {
+        arrayList.set(arrayList.indexOf(2), "Croissant");
+      } else if (i == arrayList.indexOf(false)) {
+        arrayList.set(arrayList.indexOf(false), "Ice cream");
+      }
 
-    arrayList.set(3, "Ice cream");
-    arrayList.set(1, "Croissant");
+    }
     System.out.println(arrayList);
   }
 }
