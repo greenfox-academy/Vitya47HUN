bf28751150752573ad014a3bb7d9c33da1806656
Viktor Kemeny <kemeny.viktor@hotmail.com>
2017-03-23 16:53:55 +0100

    Exercise08(Finished on second try)

--- Week02/Day04/src/Exercise08.java
+++ Week02/Day04/src/Exercise08.java
@@ -1,5 +1,14 @@
-/**
- * Created by pc on 2017.03.23..
- */
-public class Exercise08 {
+import java.util.*;
+public class Exercise08{
+  public static void main(String... args){
+    ArrayList<String> far = new ArrayList<String>(Arrays.asList("kuty", "macsk", "kacs", "rók", "halacsk"));
+
+    // Add "a" to every string in the far list.
+    int length = far.size();
+    for (int i = 0;i < length;i++){
+      far.set(i,far.get(i) + "a");
+    }
+
+    System.out.println(far);
+  }
 }
