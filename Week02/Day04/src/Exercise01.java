402ed570fa797d32b2cc1cfed42ccf3f0b81b059
Viktor Kemeny <kemeny.viktor@hotmail.com>
2017-03-23 11:51:04 +0100

    Exercise02(trying to add : to the string)

--- Week02/Day04/src/Exercise01.java
+++ Week02/Day04/src/Exercise01.java
@@ -5,8 +5,8 @@ public class Exercise01 {
   public static void main(String[] args) {
     String example = "In a dishwasher far far away";
 
-    example.replace("dishwasher", "galaxy");
+    String example2 = example.replace("dishwasher", "galaxy");
 
-    System.out.println(example.replace("dishwasher","galaxy"));
+    System.out.println(example2);
   }
 }
---
+++ Week02/Day04/src/Exercise02.java
@@ -0,0 +1,13 @@
+// Accidentally I got the wrong URL for a funny subreddit. It's probably "odds" and not "bots"
+// Also, the URL is missing a crutial component, find out what it is and insert it too!
+// Try to solve it more than once using different String functions!
+public class Exercise02 {
+  public static void main(String... args) {
+    String url = "https//www.reddit.com/r/nevertellmethebots";
+    String url2 =  url.replace("bots","odds");
+    String url3 = url.substring(6) + ":" ;
+
+    System.out.println(url3);
+  }
+}
+
