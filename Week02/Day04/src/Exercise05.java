f726aa63c208aaf96286ccc68cf7250d6e6f891c
Viktor Kemeny <kemeny.viktor@hotmail.com>
2017-03-23 14:07:28 +0100

    Exercise05(make a reverser without function, but working)

--- Week02/Day04/src/Exercise05.java
+++ Week02/Day04/src/Exercise05.java
@@ -1,2 +1,26 @@
+// Create a function that can reverse a String, which is passed as the parameter
+// Use it on this reversed string to check it!
+// Try to solve this using charAt() first, and optionally anything else after.
+
 public class Exercise05 {
+  public static void main(String... args) {
+    String reversed = ".eslaf eb t'ndluow ecnetnes siht ,dehctiws erew eslaf dna eurt fo sgninaem eht fI";
+    int length = reversed.length();
+
+
+    for (int i = length - 1; i >= 0; i--) {
+
+      System.out.print(reversed.charAt(i));
+    }
+
+
+  }
+
+
 }
+
+
+}
+
+
+
