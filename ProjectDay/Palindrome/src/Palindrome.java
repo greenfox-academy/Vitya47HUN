a052b970cff0bade938340f995ab89046ab6635c
Viktor Kemeny <kemeny.viktor@hotmail.com>
2017-03-24 11:37:35 +0100

    Made some changes to projects

--- ProjectDay/Palindrome/src/Palindrome.java
+++ ProjectDay/Palindrome/src/Palindrome.java
@@ -1,31 +1,26 @@
 import java.util.Scanner;
+
 public class Palindrome {
-    public static void main(String[] args) {
-        do {
-            Scanner scanner = new Scanner(System.in);
-            System.out.println("Welcome, I am the mighty palindrome builder, please enter a word and wait for the magic.");
-            String answer = scanner.nextLine();
-            if (answer.equals("Exit program")){
-                System.exit(0);
-            }
+  public static void main(String[] args) {
+    do {
+      Scanner scanner = new Scanner(System.in);
+      System.out.println("Welcome, I am the mighty palindrome builder, please enter a word and wait for the magic.");
+      String answer = scanner.nextLine();
+      if (answer.equals("Exit program")) {
+        System.exit(0);
+      } else {
+        System.out.println(answer + createPalindrome(answer));
+      }
+    } while (true);
+  }
 
-            else {
-
-
-                System.out.println(answer + createPalindrome(answer));
-            }
-        } while (true);
-
+  public static String createPalindrome(String index) {
+    int length = index.length();
+    String answer2 = "";
+    for (int i = length - 1; i >= 0; i--) {
+      answer2 = answer2 + index.charAt(i);
     }
-
-    public static String createPalindrome(String answer) {
-        int length = answer.length();
-        String answer2 = "";
-        for (int i = length - 1; i >= 0; i--) {
-            answer2 = answer2 + answer.charAt(i);
-        }
-
-        return answer2;
-    }
+    return answer2;
+  }
 
 }
