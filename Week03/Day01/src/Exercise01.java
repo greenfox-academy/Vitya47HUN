5ad2a57897f9848df6ebe1c1e8e29cdce0432d84
Viktor Kemeny <kemeny.viktor@hotmail.com>
2017-03-27 13:55:35 +0200

    Exercise01 almost finished, exercise02 started

--- Week03/Day01/src/Exercise01.java
+++ Week03/Day01/src/Exercise01.java
@@ -1,34 +1,49 @@
 import java.io.File;
+import java.util.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
 
 public class Exercise01 {
   public static void main(String[] args) {
-      // Create a method that find the 5 most common lotto numbers assets/lotto.csv
+    // Create a method that find the 5 most common lotto numbers assets/lotto.csv
+    Map<String, Integer> map = new HashMap<String, Integer>();
     try {
       Path filePath = Paths.get("otos.csv");
       List<String> lines = Files.readAllLines(filePath);
       List<String> numbers = new ArrayList<String>();
-      for(int i = 0;i < lines.size();i++) {
+      for (int i = 0; i < lines.size(); i++) {
 
         List<String> list = Arrays.asList(lines.get(i).split(";"));
         List<String> allNumbers = (list.subList(list.size() - 5, list.size()));
         numbers.addAll(allNumbers);
-//        System.out.println(list.subList(list.size() - 5, list.size()));
+//        System.out.println(list.subList(list.size() - 5, list.size()));x
 
       }
-      System.out.println(numbers);
 
-    }
+      for (int j = 0; j < numbers.size(); j++) {
+        Integer count = map.get(numbers.get(j));
+        map.put(numbers.get(j), count == null ? 1 : count + 1);   //auto boxing and count
+      }
 
-    catch (Exception e){
+//      System.out.println(map);
+
+      for (String key : map.keySet()) {
+        map.get(key);
+        for (int k = 0; k < key.length(); k++) {
+          int max = key.indexOf(k);
+
+          System.out.println(key);
+        }
+
+//      System.out.println(numbers);
+
+      }
+
+    } catch (Exception e) {
       e.printStackTrace();
       System.out.println("Error");
     }
+
   }
-  }
+}
---
+++ Week03/Day01/src/Exercise02.java
@@ -0,0 +1,5 @@
+/**
+ * Created by pc on 2017.03.27..
+ */
+public class Exercise02 {
+}
