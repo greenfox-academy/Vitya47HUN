3d8625e687e6926a45ba85302783cb51784f05bd
Viktor Kemeny <kemeny.viktor@hotmail.com>
2017-03-27 17:49:38 +0200

    Last commit of the day

---
+++ Week03/Day01/src/CognitiveApprenticeShip.java
@@ -0,0 +1,4 @@
+public class CognitiveApprenticeShip {
+  public static void main(String[] args) {
+  }
+}
--- Week03/Day01/src/Exercise02.java
+++ Week03/Day01/src/Exercise02.java
@@ -2,9 +2,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-
 public class Exercise02 {
   public static void main(String[] args) {
     try {
@@ -12,21 +10,15 @@ public class Exercise02 {
       List<String> lines = Files.readAllLines(filePath);
       List<String> texts = new ArrayList<String>();
 
-      // Create a method that decrypts the assets/duplicated_chars.txt
       for (int i = 0; i < lines.size(); i++) {
         String readyLine = "";
         for (int j = 0; j < lines.get(i).length(); j += 2) {
-
           readyLine = readyLine + lines.get(i).charAt(j);
-
         }
         readyLine += "\n";
         texts.add(readyLine);
-
       }
-
       System.out.print(texts);
-
     } catch (Exception e) {
       e.printStackTrace();
       System.out.println("Error");
