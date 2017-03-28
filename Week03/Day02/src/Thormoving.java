a1a7522e6d5bb7bbb590b89d9f1ee2eba3900e63
Viktor Kemeny <kemeny.viktor@hotmail.com>
2017-03-28 16:01:58 +0200

    Week03 thormovinG exercise finished, not perfect but working

--- Week03/Day02/src/Thormoving.java
+++ Week03/Day02/src/Thormoving.java
@@ -16,23 +16,56 @@ class Player {
     int lightY = in.nextInt(); // the Y position of the light of power
     int initialTX = in.nextInt(); // Thor's starting X position
     int initialTY = in.nextInt(); // Thor's starting Y position
+    int thorX = 0;
 
     // game loop
     while (true) {
-      int remainingTurns = in.nextInt(); // The remaining amount of turns Thor can move. Do not remove this line.
-      if (initialTX == lightX){
-        System.out.println("S");
+      int remainingTurns = in.nextInt();  // The remaining amount of turns Thor can move. Do not remove this line.
+
+      if (initialTY == lightY && initialTX > lightX){
+        System.out.println("W");
+        initialTX = initialTX - 1;
       }
 
-      else if (initialTX > lightX){
-        System.out.println("W");
+      else if (initialTY == lightY && initialTX < lightX){
+        System.out.println("E");
+        initialTX = initialTX + 1;
       }
+
+      else if (initialTX == lightX && initialTY > lightY){
+        System.out.println("N");
+        initialTY = initialTY - 1;
+      }
+
+      else if (initialTX == lightX && initialTY < lightY){
+        System.out.println("S");
+        initialTY = initialTY + 1;
+      }
+
+      else if (initialTX < lightX && initialTY < lightY){
+        System.out.println("SE");
+        initialTX = initialTX + 1;
+        initialTY = initialTY + 1;
+      }
+
+      else if (initialTX > lightX && initialTY > lightY){
+        System.out.println("SW");
+        initialTX = initialTX - 1;
+        initialTY = initialTY - 1;
+      }
+
+      else if (initialTX > lightX && initialTY < lightY){
+        System.out.println("SW");
+        initialTX = initialTX - 1;
+        initialTY = initialTY + 1;
+      }
+
       // Write an action using System.out.println()
       // To debug: System.err.println("Debug messages...");
-      else
 
-        // A single line providing the move to be made: N NE E SE S SW W or NW
-        System.out.println("S");
+
+      // A single line providing the move to be made: N NE E SE S SW W or NW
+
     }
   }
 }
