ad226b8eda409185757023528d52d90cbbe25216
Viktor Kemeny <kemeny.viktor@hotmail.com>
2017-03-29 10:00:52 +0200

    Started Exercise04 , created input , and integer x , y

---
+++ Week03/Day03/src/Exercise04.java
@@ -0,0 +1,40 @@
+import javax.swing.*;
+import java.awt.*;
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+import java.util.Scanner;
+
+public class Exercise04 {
+
+  public static void mainDraw(Graphics graphics){
+    // create a line drawing function that takes 2 parameters:
+    // the x and y coordinates of the line's starting point
+    // and draws a line from that point to the center of the canvas.
+    // draw 3 lines with that function.
+
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Please add X and Y starting point");
+    int x = scanner.nextInt();
+    int y = scanner.nextInt();
+
+
+  }
+
+  //    Don't touch the code below
+  public static void main(String[] args) {
+    JFrame jFrame = new JFrame("Drawing");
+    jFrame.setSize(new Dimension(300, 300));
+    jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    jFrame.add(new ImagePanel());
+    jFrame.setLocationRelativeTo(null);
+    jFrame.setVisible(true);
+  }
+  static class ImagePanel extends JPanel{
+    @Override
+    protected void paintComponent(Graphics graphics) {
+      super.paintComponent(graphics);
+      mainDraw(graphics);
+
+    }
+  }
+
+}
