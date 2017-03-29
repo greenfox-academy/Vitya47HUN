e0ea4e52e0a4edc65b3f8ae0121b14566ffb25fa
Viktor Kemeny <kemeny.viktor@hotmail.com>
2017-03-29 16:44:23 +0200

    Exercise 17 1/4 of the star form ready

---
+++ Week03/Day03/src/Exercise17.java
@@ -0,0 +1,42 @@
+import javax.swing.*;
+import java.awt.*;
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class Exercise17 {
+  public static void mainDraw(Graphics graphics) {
+    // reproduce this:
+    // [https://github.com/greenfox-academy/teaching-materials/blob/master/exercises/drawing/envelope-star/r2.png]
+
+    int xstart = 150;
+    int ystart = 150;
+    int yfinish = 0;
+    int xfinish = 150;
+    for (int l = 0; l < 15; l++) {
+      graphics.setColor(Color.GREEN);
+      graphics.drawLine(xstart, ystart, xfinish, yfinish);
+      xstart = xstart - 10;
+      yfinish = yfinish + 10;
+
+
+    }
+  }
+
+  //    Don't touch the code below
+  public static void main(String[] args) {
+    JFrame jFrame = new JFrame("Drawing");
+    jFrame.setSize(new Dimension(300, 300));
+    jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    jFrame.add(new ImagePanel());
+    jFrame.setLocationRelativeTo(null);
+    jFrame.setVisible(true);
+  }
+  static class ImagePanel extends JPanel{
+    @Override
+    protected void paintComponent(Graphics graphics) {
+      super.paintComponent(graphics);
+      mainDraw(graphics);
+
+    }
+  }
+
+}
