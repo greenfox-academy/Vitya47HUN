147ef6acdd0d6db4fcb4152255b257e2c170cf12
Viktor Kemeny <kemeny.viktor@hotmail.com>
2017-03-29 13:38:51 +0200

    Exercise10 accidently the same as exercise09, exercise08 missing

---
+++ Week03/Day03/src/Exercise10.java
@@ -0,0 +1,55 @@
+import javax.swing.*;
+import java.awt.*;
+import java.util.Scanner;
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class Exercise10 {
+
+  public static void mainDraw(Graphics graphics) {
+    // create a square drawing function that takes 2 parameters:
+    // the square size, and the fill color,
+    // and draws a square of that size and color to the center of the canvas.
+    // create a loop that fills the canvas with rainbow colored squares.
+    Scanner scanner = new Scanner(System.in);
+
+    int r = 255;
+    int g = 0;
+    int b = 0;
+    int x = 300;
+    int y = 300;
+    for(int i = 0;i < 7; i++){
+      Color rainbowColor = new Color(r ,g, b);
+      drawSquare(graphics,rainbowColor, x , y);
+      r = r - 35;
+      g = g + 35;
+      b = + b + 35;
+      x = x - 25;
+      y = y - 25;
+    }
+  }
+  public static void drawSquare(Graphics graphics,Color colorinput, int x, int y) {
+    graphics.setColor(colorinput);
+    graphics.fillRect((150 - x / 2),(150 - y / 2), x, y);
+  }
+
+
+
+  //    Don't touch the code below
+  public static void main(String[] args) {
+    JFrame jFrame = new JFrame("Drawing");
+    jFrame.setSize(new Dimension(300, 300));
+    jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    jFrame.add(new ImagePanel());
+    jFrame.setLocationRelativeTo(null);
+    jFrame.setVisible(true);
+  }
+  static class ImagePanel extends JPanel{
+    @Override
+    protected void paintComponent(Graphics graphics) {
+      super.paintComponent(graphics);
+      mainDraw(graphics);
+
+    }
+  }
+
+}
