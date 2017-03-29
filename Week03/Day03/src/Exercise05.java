c8cea5b51edbcc48b849874238cd7c9025d689f5
Viktor Kemeny <kemeny.viktor@hotmail.com>
2017-03-29 10:53:36 +0200

    Exercise05 - able to draw 1 line from userinput

--- Week03/Day03/src/Exercise05.java
+++ Week03/Day03/src/Exercise05.java
@@ -1,32 +1,31 @@
 import javax.swing.*;
 import java.awt.*;
-
+import java.util.Scanner;
 import static javax.swing.JFrame.EXIT_ON_CLOSE;
 
-import java.util.Scanner;
+public class Exercise05 {
 
-public class Exercise04 {
-
-  public static void mainDraw(Graphics graphics) {
+  public static void mainDraw(Graphics graphics){
     // create a line drawing function that takes 2 parameters:
     // the x and y coordinates of the line's starting point
-    // and draws a line from that point to the center of the canvas.
+    // and draws a 50 long horizontal line from that point.
     // draw 3 lines with that function.
+
     Scanner scanner = new Scanner(System.in);
     System.out.println("Please add X and Y starting point");
     drawLine(graphics, scanner.nextInt(), scanner.nextInt());
-    drawLine(graphics, scanner.nextInt(), scanner.nextInt());
-    drawLine(graphics, scanner.nextInt(), scanner.nextInt());
+
 
   }
 
   public static void drawLine(Graphics graphics, int x, int y) {
     graphics.setColor(Color.blue);
-    graphics.drawLine(x, y, 150, 150);
+    graphics.drawLine(x, y, x, y + 50);
   }
 
-  //    Don't touch the code below
 
+
+  //    Don't touch the code below
   public static void main(String[] args) {
     JFrame jFrame = new JFrame("Drawing");
     jFrame.setSize(new Dimension(300, 300));
@@ -35,8 +34,7 @@ public class Exercise04 {
     jFrame.setLocationRelativeTo(null);
     jFrame.setVisible(true);
   }
-
-  static class ImagePanel extends JPanel {
+  static class ImagePanel extends JPanel{
     @Override
     protected void paintComponent(Graphics graphics) {
       super.paintComponent(graphics);
