6a2d3c8f4ecb696e18ab5aa102f4dd7001794527
Viktor Kemeny <kemeny.viktor@hotmail.com>
2017-03-29 10:15:02 +0200

    Started Exercise04 , able to draw 1 line from given coordinates

--- Week03/Day03/src/Exercise04.java
+++ Week03/Day03/src/Exercise04.java
@@ -2,24 +2,33 @@ import javax.swing.*;
 import java.awt.*;
 import static javax.swing.JFrame.EXIT_ON_CLOSE;
 import java.util.Scanner;
-
 public class Exercise04 {
 
-  public static void mainDraw(Graphics graphics){
+  public static void mainDraw(Graphics graphics) {
     // create a line drawing function that takes 2 parameters:
     // the x and y coordinates of the line's starting point
     // and draws a line from that point to the center of the canvas.
     // draw 3 lines with that function.
 
-    Scanner scanner = new Scanner(System.in);
-    System.out.println("Please add X and Y starting point");
-    int x = scanner.nextInt();
-    int y = scanner.nextInt();
 
 
-  }
+      Scanner scanner = new Scanner(System.in);
+      System.out.println("Please add X and Y starting point");
+      int x = scanner.nextInt();
+      int y = scanner.nextInt();
+
+      graphics.setColor(Color.RED);
+      graphics.drawLine(x, y, 150, 150);
+
+
+
+    }
+
+
+
 
   //    Don't touch the code below
+
   public static void main(String[] args) {
     JFrame jFrame = new JFrame("Drawing");
     jFrame.setSize(new Dimension(300, 300));
@@ -28,13 +37,14 @@ public class Exercise04 {
     jFrame.setLocationRelativeTo(null);
     jFrame.setVisible(true);
   }
-  static class ImagePanel extends JPanel{
-    @Override
-    protected void paintComponent(Graphics graphics) {
-      super.paintComponent(graphics);
-      mainDraw(graphics);
 
-    }
+static class ImagePanel extends JPanel {
+  @Override
+  protected void paintComponent(Graphics graphics) {
+    super.paintComponent(graphics);
+    mainDraw(graphics);
+
   }
+}
 
 }
