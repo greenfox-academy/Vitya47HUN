2d290fd6fbcfb9f3b99e3d0260013c9cf0a75e2f
Viktor Kemeny <kemeny.viktor@hotmail.com>
2017-03-30 10:10:37 +0200

    Exercise01 finished , counting back from the given number

--- Week03/Day04/src/Exercise01.java
+++ Week03/Day04/src/Exercise01.java
@@ -1,2 +1,15 @@
 public class Exercise01 {
-}
+  public static void main(String[] args) {
+    System.out.println(countdown(15));
+  }
+  public static int countdown(int n) {
+    if(n <= 0){
+      return 0;
+    }
+    System.out.println(n);
+    return countdown(n - 1);
+    }
+
+  }
+
+
