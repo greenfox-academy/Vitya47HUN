960a3b7faeab2f62e2276aba909cd5c92737292e
Viktor Kemeny <kemeny.viktor@hotmail.com>
2017-04-03 17:56:54 +0200

    Last commit of the day

--- Week04/Day01/Exercise11/src/SharpieSet.java
+++ Week04/Day01/Exercise11/src/SharpieSet.java
@@ -1,12 +1,16 @@
-import java.util.ArrayList;
-
 //Reuse your Sharpie class
 //Create SharpieSet class
 //it contains a list of Sharpie
 //                countUsable() -> sharpie is usable if it has ink in it
 //        removeTrash() -> removes all unusable sharpies
+
+import java.util.ArrayList;
+
 public class SharpieSet {
-  ArrayList<String> sharpies;
+  ArrayList<Sharpie> sharpies;
+
+  public void countUsable(){
+  }
 }
 
 
