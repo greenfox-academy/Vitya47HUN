46eb94cacbf184d065e7d55888d1b9fa8353420f
Viktor Kemeny <kemeny.viktor@hotmail.com>
2017-04-03 11:52:13 +0200

    Exercise03 , base of the exercise got, dominoes are not in correct order

--- Week04/Day01/Exercise03/src/Dominoes.java
+++ Week04/Day01/Exercise03/src/Dominoes.java
@@ -1,10 +1,18 @@
-import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Dominoes {
-  ArrayList<Integer> dominoNumber;
+  private final int[] values;
 
+  public Dominoes(int valueA, int valueB) {
+    this.values = new int[]{valueA, valueB};
+  }
 
-  public Dominoes {
-    this.dominoNumber = dominoNumber;
+  public int[] getValues() {
+    return values;
+  }
+
+  @Override
+  public String toString() {
+    return "[" + values[0] + ", " + values[1] + "]";
   }
 }
--- Week04/Day01/Exercise03/src/Main.java
+++ Week04/Day01/Exercise03/src/Main.java
@@ -1,8 +1,24 @@
-//You have the list of Dominoes
-//        Order them into one snake where the adjacent dominoes have the same numbers on their adjacent sides
-//        eg: [2, 4], [4, 3], [3, 5] ...
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
   public static void main(String[] args) {
-  Dominoes dominoearray = new Dominoes ([2][4]);
+    List<Dominoes> dominoes = initializeDominoes();
+    // You have the list of Dominoes
+    // Order them into one snake where the adjacent dominoes have the same numbers on their adjacent sides
+    // eg: [2, 4], [4, 3], [3, 5] ...
+
+    System.out.println(dominoes);
+  }
+
+  static List<Dominoes> initializeDominoes() {
+    List<Dominoes> dominoes = new ArrayList<>();
+    dominoes.add(new Dominoes(5, 2));
+    dominoes.add(new Dominoes(4, 6));
+    dominoes.add(new Dominoes(1, 5));
+    dominoes.add(new Dominoes(6, 7));
+    dominoes.add(new Dominoes(2, 4));
+    dominoes.add(new Dominoes(7, 1));
+    return dominoes;
   }
 }
