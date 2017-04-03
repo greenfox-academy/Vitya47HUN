587ffb5b7a1a9fad1410c20c933691435e559b88
Viktor Kemeny <kemeny.viktor@hotmail.com>
2017-04-03 16:11:11 +0200

    Exercise09 finished

--- Week04/Day01/Exercise09/src/Student.java
+++ Week04/Day01/Exercise09/src/Student.java
@@ -1,20 +1,8 @@
-//Teacher Student
-//
-//        Create Student and Teacher classes
-//        Student
-//        learn()
-//        question(teacher) -> calls the teachers answer method
-//        Teacher
-//        teach(student) -> calls the students learn method
-//        answer()
 public class Student {
-  String question;
-
-  public void question(String teacherName) {
-    teacherName = question;
+  public void learn() {
   }
 
-  public void learn(String question) {
-    this.question = question;
+  public void question(Teacher teacher) {
+    teacher.answer();
   }
 }
--- Week04/Day01/Exercise09/src/Teacher.java
+++ Week04/Day01/Exercise09/src/Teacher.java
@@ -1,9 +1,8 @@
 public class Teacher {
-  public static void main(String[] args) {
-    Student Bobby = new Student();
-    Bobby.learn("What is the meaning of life?");
+  public void answer() {
   }
-  public teach(){
 
+  public void teach(Student student) {
+    student.learn();
   }
 }
