0c3110531af3d148c2771ed6b4b0c235fa253b1d
Viktor Kemeny <kemeny.viktor@hotmail.com>
2017-04-03 11:21:02 +0200

    Exercise03 started

---
+++ Week04/Day01/Exercise03/src/Main.java
@@ -0,0 +1,8 @@
+//You have the list of Dominoes
+//        Order them into one snake where the adjacent dominoes have the same numbers on their adjacent sides
+//        eg: [2, 4], [4, 3], [3, 5] ...
+public class Main {
+  public static void main(String[] args) {
+
+  }
+}
--- Week04/Day01/src/Exercise02.java
+++ Week04/Day01/src/Exercise02.java
@@ -1,6 +1,6 @@
 public class Exercise02 {
   public static void main(String[] args) {
-    BlogPost post1 = new BlogPost("John Doe","Lorem Ipsum","Asdasdasd","2011");
+    BlogPost post1 = new BlogPost("John Doe","Lorem Ipsum","Lorem ipsum dolor sit amet","2011");
 
     System.out.println(post1);
   }
