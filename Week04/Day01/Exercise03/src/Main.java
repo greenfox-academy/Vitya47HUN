b6797ac2da50a984b041311d2beed400963296fb
Viktor Kemeny <kemeny.viktor@hotmail.com>
2017-04-03 12:14:44 +0200

    Exercise03 finished, commented the alternative, algorythm sorting solution

--- Week04/Day01/Exercise03/src/Main.java
+++ Week04/Day01/Exercise03/src/Main.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Collections;
 
 public class Main {
   public static void main(String[] args) {
@@ -8,17 +9,30 @@ public class Main {
     // Order them into one snake where the adjacent dominoes have the same numbers on their adjacent sides
     // eg: [2, 4], [4, 3], [3, 5] ...
 
-    System.out.println(dominoes);
+    System.out.println((dominoes));
   }
 
-  static List<Dominoes> initializeDominoes() {
-    List<Dominoes> dominoes = new ArrayList<>();
-    dominoes.add(new Dominoes(5, 2));
-    dominoes.add(new Dominoes(4, 6));
-    dominoes.add(new Dominoes(1, 5));
-    dominoes.add(new Dominoes(6, 7));
-    dominoes.add(new Dominoes(2, 4));
-    dominoes.add(new Dominoes(7, 1));
-    return dominoes;
+//  public static List<Dominoes> adjacent(List<Dominoes> input) {
+//    for (int i = 0; i < input.size(); i++) {
+//      int[] value1 = input.get(i).getValues();
+//      for (int j = i + 1; j < input.size(); j++) {
+//        int[] value2 = input.get(j).getValues();
+//        if (value1[1] == value2[0]) {
+//          Collections.swap(input, i + 1, j);
+//        }
+//      }
+//    }
+//      return input;
+//    }
+
+    static List<Dominoes> initializeDominoes () {
+      List<Dominoes> dominoes = new ArrayList<>();
+      dominoes.add(new Dominoes(5, 2));
+      dominoes.add(new Dominoes(2, 4));
+      dominoes.add(new Dominoes(4, 6));
+      dominoes.add(new Dominoes(6, 7));
+      dominoes.add(new Dominoes(7, 1));
+      dominoes.add(new Dominoes(1, 5));
+      return dominoes;
+    }
   }
-}
