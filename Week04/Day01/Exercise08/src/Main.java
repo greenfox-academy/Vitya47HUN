f96cc82efc0a4ca82bf5b76d2986320a30fe853f
Viktor Kemeny <kemeny.viktor@hotmail.com>
2017-04-03 15:35:20 +0200

    Exercise08 started made some basic methods with add()

---
+++ Week04/Day01/Exercise08/src/Main.java
@@ -0,0 +1,9 @@
+import java.util.Collections;
+
+public class Main {
+  public static void main(String[] args) {
+    Counter num = new Counter(5);
+    num.get();
+    System.out.println(num);
+  }
+}
