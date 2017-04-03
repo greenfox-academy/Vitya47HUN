23c7794d01ecec3f8b5451ea51ccfa9e1175f594
Viktor Kemeny <kemeny.viktor@hotmail.com>
2017-04-03 11:14:12 +0200

    Exercise02 used toString method, printed out 1 BlogPost

--- Week04/Day01/src/BlogPost.java
+++ Week04/Day01/src/BlogPost.java
@@ -1,3 +1,24 @@
 public class BlogPost {
-  
+  String authorName;
+  String title;
+  String text;
+  String publicationDate;
+
+  @Override
+  public String toString() {
+    return "BlogPost{" +
+            "authorName='" + authorName + '\'' +
+            ", title='" + title + '\'' +
+            ", text='" + text + '\'' +
+            ", publicationDate='" + publicationDate + '\'' +
+            '}';
+  }
+
+  public BlogPost(String authorName, String title, String text, String publicationDate){
+    this.authorName = authorName;
+    this.title = title;
+    this.text = text;
+    this.publicationDate = publicationDate;
+
+  }
 }
--- Week04/Day01/src/Exercise02.java
+++ Week04/Day01/src/Exercise02.java
@@ -1,5 +1,8 @@
 public class Exercise02 {
   public static void main(String[] args) {
+    BlogPost post1 = new BlogPost("John Doe","Lorem Ipsum","Asdasdasd","2011");
 
+    System.out.println(post1);
   }
+
 }
