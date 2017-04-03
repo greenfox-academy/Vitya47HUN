244b9cc48e428c0059cc53259357af6601ea2e1e
Viktor Kemeny <kemeny.viktor@hotmail.com>
2017-04-03 14:55:11 +0200

    Exercise06 made some progress with hunger and thrist, it asks for number , calculates it and prints out how hungry is the animal

--- Week04/Day01/Exercise06/src/Animal.java
+++ Week04/Day01/Exercise06/src/Animal.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 //Create Animal class
 //Every animal has a hunger value, which is a whole number
 //                Every animal has a thirst value, which is a whole number
@@ -9,6 +11,8 @@ public class Animal {
   String name;
   int hunger;
   int thirst;
+  int eat;
+  int drink;
 
   public Animal() {
     this("unkown", 0, 0);
@@ -20,12 +24,27 @@ public class Animal {
     this.thirst = thirst;
   }
 
+  public Animal(int eat, int drink, int play){
+    this.eat = eat;
+    this.drink = drink;
+  }
+
   public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
     Animal gepard = new Animal();
     Animal turtle = new Animal();
     gepard.thirst = 50;
     gepard.hunger = 50;
+    gepard.drink = 1;
 
-    System.out.println(gepard);
+    System.out.println("Feed the gepard");
+    int feeding = scanner.nextInt();
+    gepard.eat = feeding;
+    System.out.println("Gepard is : " + (gepard.hunger - gepard.eat) + "% hungry");
+
+    System.out.println("Give some water to the gepard");
+    int drinking = scanner.nextInt();
+    gepard.thirst = gepard.thirst - drinking;
+    System.out.println(gepard.thirst);
   }
 }
