91e49d013a1707cba6e5de267bebd7dee890cd10
Viktor Kemeny <kemeny.viktor@hotmail.com>
2017-04-04 17:18:48 +0200

    Exercise02 it splits the water correcrtly between flowers but the trees still remaining, and it shows all flowers

--- Week04/Day02/Exercise02/src/Garden.java
+++ Week04/Day02/Exercise02/src/Garden.java
@@ -11,23 +11,32 @@ public class Garden {
     allFlowers.add(tulip);
     Flowers pitypang = new Flowers("green", 3, 5);
     allFlowers.add(pitypang);
+    Flowers szotyi = new Flowers("red", 6, 5);
+    allFlowers.add(szotyi);
 
-    int thristyPlants = 0;
+    double thristyPlants = 0.0;
 
     for (int i = 0; i < allFlowers.size(); i++) {
       if (allFlowers.get(i).waterlevel < 5) {
-        thristyPlants += 1;
+        thristyPlants += 1.0;
       }
-      for (int j = 0; j < allFlowers.size(); j++) {
-        if (allFlowers.get(j).waterlevel < 5){
-          allFlowers.get(j).watering(6 / thristyPlants);
-        }
-      }
-      System.out.println("The flower : " + allFlowers.get(i));
+
     }
-    System.out.println(thristyPlants);
+    for (int j = 0; j < allFlowers.size(); j++) {
+      if (allFlowers.get(j).waterlevel < 5) {
+        allFlowers.get(j).watering(6.0 / thristyPlants);
+
+
+      }
+    }
+    for (int i = 0; i < allFlowers.size(); i++) {
+      System.out.println(allFlowers.get(i));
+    }
   }
 }
+
+  
+
 
 
 
