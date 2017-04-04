817d235ed628a46eaae89e0dd15c18bd0b606dd8
Viktor Kemeny <kemeny.viktor@hotmail.com>
2017-04-04 11:31:23 +0200

    Started first eercise, added some basic variable

---
+++ Week04/Day02/Exercise01/src/Person.java
@@ -0,0 +1,9 @@
+public class Person {
+  private String name;
+  private int age;
+  private String gender;
+
+  public void introduce(){
+    System.out.println("Hi, I'm " + name + ", a " + age + " years old " + gender + ".");
+  }
+}
