d6c988c461a3dc572797538cc8c47c7497ac8b6b
Viktor <Kemeny.viktor@hotmail.com>
2017-04-04 20:23:09 +0200

    FINALLY garden exercise prototype finished, it can show which plant needs to be watered, separated correctly BUT it needs polishing and making correct methods

--- Week04/Day02/Exercise02/src/Plants.java
+++ Week04/Day02/Exercise02/src/Plants.java
@@ -10,4 +10,17 @@ public class Plants {
   public void setcolor(String color){
     this.color = color;
   }
+
+
+  public boolean isNeed() {
+    return (this.waterlevel < this.waterlimit);
+  }
+
+  public void info() {
+    if (isNeed()) {
+      System.out.println("need water");
+    } else {
+      System.out.println("dont need water");
+    }
+  }
 }
