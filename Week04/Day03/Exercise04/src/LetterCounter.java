fb52572f2d7a1be590bc1a408ba2e573efdc18d5
Viktor Kemeny <kemeny.viktor@hotmail.com>
2017-04-05 14:23:35 +0200

    Exercise04 it stores how many occurances a letter has

--- Week04/Day03/Exercise04/src/LetterCounter.java
+++ Week04/Day03/Exercise04/src/LetterCounter.java
@@ -12,11 +12,12 @@ public class LetterCounter {
   public static Map<String, Integer> letterCounter(String letters){
     Map <String, Integer> letterMap = new HashMap<>();
     for (int i = 0; i < letters.length();i++){
-      if(letterMap.containsKey(letters.charAt(i))){
-        letterMap.put(letters.substring(i,i+1),i + 1);
+      if(letterMap.containsKey(letters.substring(i, i + 1))){
+        letterMap.put(letters.substring(i,i+1),letterMap.get(letters.substring(i,i+1)) + 1);
       }
-
-      letterMap.put(letters.substring(i,i+1),1);
+      else {
+        letterMap.put(letters.substring(i, i + 1), 1);
+      }
     }
     return letterMap;
   }
