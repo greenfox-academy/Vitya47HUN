fb436227fdfb8c611b6a5b16fdbec397df3f375d
Viktor Kemeny <kemeny.viktor@hotmail.com>
2017-04-05 14:14:21 +0200

    Exercise04 it can store the letters and numbers into a hashmap, trying to get the value number as much as the letters occurance

--- Week04/Day03/Exercise04/src/LetterCounter.java
+++ Week04/Day03/Exercise04/src/LetterCounter.java
@@ -1,12 +1,23 @@
+import java.util.HashMap;
 import java.util.Map;
 
 public class LetterCounter {
   public static void main(String[] args) {
-    String letters = "abcdefgh";
-    System.out.println(letters);
+    String letters = "abcdefghhh";
+    System.out.println(letterCounter(letters));
+
+
   }
 
   public static Map<String, Integer> letterCounter(String letters){
+    Map <String, Integer> letterMap = new HashMap<>();
+    for (int i = 0; i < letters.length();i++){
+      if(letterMap.containsKey(letters.charAt(i))){
+        letterMap.put(letters.substring(i,i+1),i + 1);
+      }
 
+      letterMap.put(letters.substring(i,i+1),1);
+    }
+    return letterMap;
   }
 }
