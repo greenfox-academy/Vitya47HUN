58d34c7e22726099e9ba2d0f70e1568020197605
Viktor Kemeny <kemeny.viktor@hotmail.com>
2017-04-05 12:20:10 +0200

    Exercise02 changed the program entirely, its working now

--- Week04/Day03/Exercise02/src/Sum.java
+++ Week04/Day03/Exercise02/src/Sum.java
@@ -1,13 +1,29 @@
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class Sum {
-  ArrayList <Integer> mylist = new ArrayList<Integer>();
-  int sum = 0;
+  public static void main(String[] args) {
+    ArrayList<Integer> listNumbers = new ArrayList<>();
+    listNumbers.add(1);
+    listNumbers.add(2);
+    listNumbers.add(3);
+    listNumbers.add(4);
+//    Collections.addAll(listNumbers, 1, 2, 3, 4);  < --- ezzel gyorsabb!
+    System.out.println(listNumbers);
+    System.out.println(sumTogether(listNumbers));
 
-  public int sumNumbers(){
-    for (int i = 0; i < mylist.size(); i++){
-      sum = sum + mylist.indexOf(i);
+  }
+
+  public static int sumTogether(ArrayList numbers) {
+    if (numbers == null) {
+      return 0;
     }
-    return sum;
+    else {
+      int sum = 0;
+      for (int i = 0; i < numbers.size(); i++) {
+        sum += (int) numbers.get(i);
+      }
+      return sum;
+    }
   }
 }
--- Week04/Day03/Exercise02/test/SumTest.java
+++ Week04/Day03/Exercise02/test/SumTest.java
@@ -1,12 +1,24 @@
 import org.junit.Test;
-
+import java.util.ArrayList;
 import static org.junit.Assert.*;
 
 public class SumTest {
-  static int sum;
+  Sum testSumClass = new Sum();
+
   @Test
-  public void testGetSum() throws Exception {
-    Sum mySum = new Sum();
-    assertEquals(sum, mySum.sum);
+  public void testSumMethod1() throws Exception {
+    ArrayList<Integer> numbers = new ArrayList<>();
+    numbers.add(1);
+    numbers.add(2);
+    numbers.add(3);
+
+    assertEquals(6, testSumClass.sumTogether(numbers));
+  }
+
+  @Test
+  public void testSumMethod2() throws Exception {
+
+    ArrayList<Integer> numbers = null;
+    assertEquals(0,  testSumClass.sumTogether(numbers));
   }
 }
