798a2b913e2735e68d61074f6c420ea38023b5d3
Viktor Kemeny <kemeny.viktor@hotmail.com>
2017-04-05 10:45:06 +0200

    Exercise01 started, trying to make the test working

---
+++ Week04/Day03/Exercise01/test/ApplesTest.java
@@ -0,0 +1,14 @@
+import org.junit.After;
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+@Before
+
+public class ApplesTest {
+  public void testApple(){
+    assertEquals("apples");
+  }
+}
+
+@After
