bb37aaaf4d0b761a0f54a1ce32b3c41d4eb180d7
Viktor Kemeny <kemeny.viktor@hotmail.com>
2017-04-05 15:47:19 +0200

    Exercise06 max of three method fixed, median is almost fixed

--- Week04/Day03/Exercise06/src/Extension.java
+++ Week04/Day03/Exercise06/src/Extension.java
@@ -3,20 +3,20 @@ import java.util.List;
 
 public class Extension {
   int add(int a, int b) {
-    return 5;
+    return a+b;
   }
 
   int maxOfThree(int a, int b, int c) {
-    if (a > b && a > c)
+    if (a > b && a > c) {
       return a;
-    else if( b > a && b > c)
+    } else if (b > a && b > c) {
       return b;
-    else if (c > b && c > a)
+    } else
       return c;
   }
 
-  int median(List<Integer> pool) {
-    return pool.get((pool.size()-1)/2);
+  double median(List<Integer> pool) {
+    return pool.get((pool.size() - 1) % 2);
   }
 
   boolean isVowel(char c) {
@@ -29,9 +29,9 @@ public class Extension {
     for (int i = 0; i < length; i++) {
       char c = teve.charAt(i);
       if (isVowel(c)) {
-        teve = String.join(c + "v" + c, teve.split(""+c));
-        i+=2;
-        length+=2;
+        teve = String.join(c + "v" + c, teve.split("" + c));
+        i += 2;
+        length += 2;
       }
     }
     return teve;
--- Week04/Day03/Exercise06/test/ExtensionTest.java
+++ Week04/Day03/Exercise06/test/ExtensionTest.java
@@ -31,12 +31,12 @@ class ExtensionTest {
 
   @Test
   void testMedian_four() {
-    assertEquals(5, extension.median(Arrays.asList(7,5,3,5)));
+    assertEquals(5, (int)extension.median(Arrays.asList(7,5,3,5)));
   }
 
   @Test
   void testMedian_five() {
-    assertEquals(3, extension.median(Arrays.asList(1,2,3,4,5)));
+    assertEquals(3, (int)extension.median(Arrays.asList(3,2,3,4,5)));
   }
 
   @Test
