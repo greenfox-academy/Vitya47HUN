b3b6e42cbc37cfe5247b3a0a7503fafb9bd8ab5d
Viktor Kemeny <kemeny.viktor@hotmail.com>
2017-04-05 11:22:40 +0200

    Exercise02 the test gives back true statement, but I am unsure that it works properly

--- Week04/Day03/Exercise02/test/SumTest.java
+++ Week04/Day03/Exercise02/test/SumTest.java
@@ -3,9 +3,10 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class SumTest {
+  static int sum;
   @Test
   public void testGetSum() throws Exception {
     Sum mySum = new Sum();
-    assertEquals(mySum, mySum.sum);
+    assertEquals(sum, mySum.sum);
   }
 }
