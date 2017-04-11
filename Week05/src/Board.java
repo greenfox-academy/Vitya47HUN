55a71dd8e5367875d8bb0de19fdc946993be00d3
Viktor Kemeny <kemeny.viktor@hotmail.com>
2017-04-11 10:10:31 +0200

    Week05 project separated to 3 class

--- Week05/src/Board.java
+++ Week05/src/Board.java
@@ -23,24 +23,8 @@ public class Board extends JComponent implements KeyListener {
     graphics.fillRect(testBoxX, testBoxY, 100, 100);
     // here you have a 720x720 canvas
     // you can create and draw an image using the class below e.g.
-    PositionedImage image = new PositionedImage("yourimage.png", 300, 300);
+    PositionedImage image = new PositionedImage("hero-down.png", 300, 300);
     image.draw(graphics);
-  }
-
-  public static void main(String[] args) {
-    // Here is how you set up a new window and adding our board to it
-    JFrame frame = new JFrame("RPG Game");
-    Board board = new Board();
-    frame.add(board);
-    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    frame.setVisible(true);
-    frame.pack();
-    // Here is how you can add a key event listener
-    // The board object will be notified when hitting any key
-    // with the system calling one of the below 3 methods
-    frame.addKeyListener(board);
-    // Notice (at the top) that we can only do this
-    // because this Board class (the type of the board object) is also a KeyListener
   }
 
   // To be a KeyListener the class needs to have these 3 methods in it
