ba1c56b857fdb15bbab4a27c63a2be9cfd7ac4b4
Viktor <Kemeny.viktor@hotmail.com>
2017-04-16 13:38:50 +0200

    week3 exercise03 started

---
+++ Week03/Day01/src/Exercise03.java
@@ -0,0 +1,9 @@
+// Write a function that takes a filename as string,
+// then returns the number of lines the file contains.
+// It should return zero if it can't open the file, and
+// should not raise any error.
+public class Exercise03 {
+  public static void main(String[] args) {
+
+  }
+}
