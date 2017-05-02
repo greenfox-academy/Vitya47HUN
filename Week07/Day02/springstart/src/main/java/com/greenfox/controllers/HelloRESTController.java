0bf4031de493fde2e4911403c3d9dd33674e969f
Viktor Kemeny <kemeny.viktor@hotmail.com>
2017-05-02 14:11:10 +0200

    HelloWebController has now 2 method 1 for the name and 1 for the number of times u visited the website

--- Week07/Day02/springstart/src/main/java/com/greenfox/controllers/HelloRESTController.java
+++ Week07/Day02/springstart/src/main/java/com/greenfox/controllers/HelloRESTController.java
@@ -2,7 +2,6 @@ package com.greenfox.controllers;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
 import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
--- Week07/Day02/springstart/src/main/java/com/greenfox/controllers/HelloWebController.java
+++ Week07/Day02/springstart/src/main/java/com/greenfox/controllers/HelloWebController.java
@@ -2,12 +2,21 @@ package com.greenfox.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import java.util.concurrent.atomic.AtomicLong;
 
 @Controller
 public class HelloWebController {
   @RequestMapping("/web/greeting")
-  public String greeting(Model model) {
-    model.addAttribute("name", " World");
+  public String greeting(Model model,@RequestParam("name") String name) {
+    model.addAttribute("name", " " + name);
     return "greeting";
   }
+
+  public AtomicLong greetingnum(Model model){
+   aLong.getAndIncrement();
+   return aLong;
+  }
+
+  AtomicLong aLong = new AtomicLong(1);
 }
