2a6d6fe134a3e6b2b632063b66793ec0c4945f01
Viktor Kemeny <kemeny.viktor@hotmail.com>
2017-05-02 10:59:16 +0200

    Added controller package

--- Week07/Day02/springstart/src/main/java/com/greenfox/SpringstartApplication.java
+++ Week07/Day02/springstart/src/main/java/com/greenfox/SpringstartApplication.java
@@ -2,11 +2,22 @@ package com.greenfox;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
+@Controller
 public class SpringstartApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringstartApplication.class, args);
 	}
+
+	@ResponseBody
+	@RequestMapping(value="/hello")
+	public String hello(){
+		return "Hello world";
+	}
+
 }
