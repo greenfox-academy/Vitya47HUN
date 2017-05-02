653d79b49ad9f2a2fc4510a2c74e4766b20abd17
Viktor Kemeny <kemeny.viktor@hotmail.com>
2017-05-02 12:13:02 +0200

    Greetings method working, now trying to get your name and greet you

--- Week07/Day02/springstart/src/main/java/com/greenfox/controllers/HelloRESTController.java
+++ Week07/Day02/springstart/src/main/java/com/greenfox/controllers/HelloRESTController.java
@@ -1,6 +1,15 @@
 package com.greenfox.controllers;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloRESTController {
+
+  @RequestMapping
+  public Greetings greeting(@RequestParam("name") String name){
+    Greetings newGreet = new Greetings(1,"Hello Your name");
+    return newGreet;
+  }
+
 }
