246b37f8bb7cb7a3b9a4dfbf72e29f6b36f29f0d
Viktor Kemeny <kemeny.viktor@hotmail.com>
2017-05-03 13:40:43 +0200

    Exercise4 starterd, it gives error when i try to type utext

--- Week07/Day03/lionking/src/main/java/com/greenfoxacademy/controller/Exercise4.java
+++ Week07/Day03/lionking/src/main/java/com/greenfoxacademy/controller/Exercise4.java
@@ -5,21 +5,20 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
 import java.text.DecimalFormat;
 
 
 @Controller
-public class Exercise2 {
+public class Exercise4 {
   BankAccount newAccount = new BankAccount("Simba", 2000.000, "lion");
   DecimalFormat formater = new DecimalFormat("0.00");
 
 
-  @RequestMapping("getBankAccount2")
-  public String getBankAccount(Model model, @RequestParam("name") String name) {
+  @RequestMapping("thisText")
+  public String thisText(Model model, @RequestParam("name") String name) {
     model.addAttribute("name",newAccount.getName());
     model.addAttribute("balance",formater.format(newAccount.getBalance()));
     model.addAttribute("type",newAccount.getAnimalType());
-    return "getBankAccount2";
+    return "thisText";
   }
 }
