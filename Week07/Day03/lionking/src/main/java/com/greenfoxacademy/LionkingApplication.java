7db9cbe4b7208cba45749780f8b6a1c3e6c6d186
Viktor Kemeny <kemeny.viktor@hotmail.com>
2017-05-03 11:30:28 +0200

    First exercise started, generated the main files and folders

---
+++ Week07/Day03/lionking/src/main/java/com/greenfoxacademy/LionkingApplication.java
@@ -0,0 +1,12 @@
+package com.greenfoxacademy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LionkingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LionkingApplication.class, args);
+	}
+}
