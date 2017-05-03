39facdc791cc2a7af94e48d0d8abbb376e3951f0
Viktor Kemeny <kemeny.viktor@hotmail.com>
2017-05-03 11:52:30 +0200

    First exercise, controller now got a model, and gets the variants of the newAccount

--- Week07/Day03/lionking/src/main/java/com/greenfoxacademy/controller/Exercise1.java
+++ Week07/Day03/lionking/src/main/java/com/greenfoxacademy/controller/Exercise1.java
@@ -2,15 +2,20 @@ package com.greenfoxacademy.controller;
 import com.greenfoxacademy.model.BankAccount;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @SpringBootApplication
 @Controller
 public class Exercise1 {
-  @RequestMapping
-  public BankAccount getBankAccount(@RequestParam("account") String name) {
-    BankAccount newAccount = new BankAccount("Simba", 2000, "lion");
-    return newAccount;
+  BankAccount newAccount = new BankAccount("Simba", 2000, "lion");
+
+  @RequestMapping("account")
+  public String getBankAccount(Model model, @RequestParam("account") String name) {
+    model.addAttribute("name",newAccount.getName());
+    model.addAttribute("balance",newAccount.getBalance());
+    model.addAttribute("type",newAccount.getAnimalType());
+    return "getBankAccount";
   }
 }
