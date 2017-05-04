2098b25202bd60bf1206f356b6503344aab20d5a
Viktor <Kemeny.viktor@hotmail.com>
2017-05-05 00:43:58 +0200

    It can now store water and food to the menu which is in the fox class for now, and it shows nothing if the menu is empty but it doesnt show the menu list when its not empty

--- Week07/Day05/foxclub/src/main/java/com/foxclub/controller/MainController.java
+++ Week07/Day05/foxclub/src/main/java/com/foxclub/controller/MainController.java
@@ -1,6 +1,7 @@
 package com.foxclub.controller;
 
 import com.foxclub.model.Fox;
+import com.foxclub.model.Menu;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,6 +14,8 @@ public class MainController {
 
   @Autowired
   private Fox foxy;
+  private Menu menu;
+
 
   @RequestMapping("/")
   public String index(Model model) {
@@ -20,20 +23,29 @@ public class MainController {
 //    foxy.setFood("A");
 //    foxy.add("Jump");
     model.addAttribute("fox",foxy);
+//    model.addAttribute("menu",menu);
     return "index";
   }
 
   @RequestMapping("/nutritionstore")
-  public String nutrition(Model model1) {
+  public String nutrition(Model model) {
+    model.addAttribute("fox",foxy);
     return "nutritionstore";
   }
 
-  @RequestMapping("/changenutrition")
+  @RequestMapping("/addnutrition")
   public String changenutrition(@RequestParam String food,String drink) {
-    foxy.setFood(food);
-    foxy.setDrink(drink);
+    foxy.addfood(food);
+    foxy.adddrink(drink);
     return "redirect:";
   }
+
+//  @RequestMapping("/changenutrition")
+//  public String addnutrition(@RequestParam String food,String drink) {
+//    foxy.setFood(food);
+//    foxy.setDrink(drink);
+//    return "redirect:";
+//  }
 
 
 }
---
+++ Week07/Day05/foxclub/src/main/java/com/foxclub/model/Menu.java
@@ -0,0 +1,32 @@
+package com.foxclub.model;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Menu {
+  private List<String> foods = new ArrayList<>();
+  private List<String> drinks = new ArrayList<>();
+
+
+  public void addfood(final String input) {
+    foods.add(input);
+  }
+
+  public void adddrink(final String input) {
+    drinks.add(input);
+  }
+
+  public int getfoodsSize() {
+    return foods.size();
+  }
+
+  public int getdrinksSize() {
+    return drinks.size();
+  }
+
+  public List<String> getDrinks() {
+    return drinks;
+  }
+  public List<String> getFoods() {
+    return foods;
+  }
+}
