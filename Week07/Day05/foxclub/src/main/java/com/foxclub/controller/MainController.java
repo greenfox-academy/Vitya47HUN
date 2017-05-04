5f5b9410561fa1086ce71f425e15873024615f6d
Viktor <Kemeny.viktor@hotmail.com>
2017-05-04 23:50:36 +0200

    It can now add Food and Water

--- Week07/Day05/foxclub/src/main/java/com/foxclub/controller/MainController.java
+++ Week07/Day05/foxclub/src/main/java/com/foxclub/controller/MainController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
@@ -15,9 +16,9 @@ public class MainController {
 
   @RequestMapping("/")
   public String index(Model model) {
-    foxy.setName("Foxyka");
-    foxy.setFood("Rabbit");
-    foxy.add("Jump");
+//    foxy.setName("A");
+//    foxy.setFood("A");
+//    foxy.add("Jump");
     model.addAttribute("fox",foxy);
     return "index";
   }
@@ -26,5 +27,13 @@ public class MainController {
   public String nutrition(Model model1) {
     return "nutritionstore";
   }
+
+  @RequestMapping("/changenutrition")
+  public String changenutrition(@RequestParam String food,String drink) {
+    foxy.setFood(food);
+    foxy.setDrink(drink);
+    return "redirect:";
+  }
+
 
 }
--- Week07/Day05/foxclub/src/main/java/com/foxclub/model/Fox.java
+++ Week07/Day05/foxclub/src/main/java/com/foxclub/model/Fox.java
@@ -9,6 +9,7 @@ import java.util.List;
 public class Fox {
   private String name;
   private String food;
+  private String drink;
   private List<String> trick = new ArrayList<>();
 
   public void add(final String input){
@@ -18,4 +19,30 @@ public class Fox {
   public int getTrickSize(){
     return trick.size();
   }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getFood() {
+    return food;
+  }
+
+  public void setFood(String food) {
+    this.food = food;
+  }
+
+
+  public String getDrink() {
+    return drink;
+  }
+
+  public void setDrink(String drink) {
+    this.drink = drink;
+  }
+
 }
