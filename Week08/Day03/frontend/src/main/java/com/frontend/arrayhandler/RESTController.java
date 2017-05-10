a4468c0fc21c1d4e611ede0bb679095b6f3a5252
Viktor <Kemeny.viktor@hotmail.com>
2017-05-10 19:48:56 +0200

    Toarray exercise done

--- Week08/Day03/frontend/src/main/java/com/frontend/arrayhandler/RESTController.java
+++ Week08/Day03/frontend/src/main/java/com/frontend/arrayhandler/RESTController.java
@@ -14,21 +14,15 @@ public class RESTController {
   public ResultObject calculate(@RequestBody InputObject input) {
     ResultObject result = new ResultObject();
     if (input.what.equals("sum")) {
-      for (int i = 0; i < input.numbers.size(); i++) {
-        result.result += input.numbers.get(i);
+      result.sum(input.getNumbers());
       }
+      else if(input.what.equals("multiply")){
+      result.multiply(input.getNumbers());
     }
-    if (input.what.equals("multiply")) {
-      result.setResult(1);
-      for (int j = 0; j < input.numbers.size(); j++) {
-        result.result = result.result * input.numbers.get(j);
-      }
-    }
-    if (input.what.equals("double")) {
-      for (int j = 0; j < input.numbers.size(); j++) {
-
-      }
+    else if(input.what.equals("double")){
+        result.toDouble(input.getNumbers());
     }
     return result;
+    }
   }
-}
+
--- Week08/Day03/frontend/src/main/java/com/frontend/arrayhandler/ResultObject.java
+++ Week08/Day03/frontend/src/main/java/com/frontend/arrayhandler/ResultObject.java
@@ -1,20 +1,42 @@
 package com.frontend.arrayhandler;
 
+import java.util.List;
+
 public class ResultObject {
-  int result;
+  Object result;
 
   public ResultObject() {
   }
 
-  public ResultObject(int result) {
+  public void setResult(int result) {
     this.result = result;
   }
 
-  public int getResult() {
+  public Object getResult() {
     return result;
   }
 
-  public void setResult(int result) {
-    this.result = result;
+  public void sum(List<Integer> numbers) {
+    int tempNumber = 0;
+    for (int i : numbers) {
+      tempNumber = tempNumber + i;
+    }
+    this.result = tempNumber;
+  }
+
+  public void multiply(List<Integer> numbers) {
+    int tempNumber = 1;
+    for (int i : numbers) {
+      tempNumber = tempNumber * i;
+    }
+    this.result = tempNumber;
+  }
+
+  public void toDouble(List<Integer> numbers) {
+    int[] tempNumber = new int[numbers.size()];
+    for  (int i = 0; i < numbers.size(); i++) {
+      tempNumber[i] = numbers.get(i) * 2;
+    }
+    this.result = tempNumber;
   }
 }
