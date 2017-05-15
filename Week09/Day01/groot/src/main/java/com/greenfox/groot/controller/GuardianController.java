0289a722c3aa49cc43e423fba5eefff36b3ba3fe
Viktor Kemeny <kemeny.viktor@hotmail.com>
2017-05-15 11:43:02 +0200

    First exercise test is not giving back green, it gets 404error

--- Week09/Day01/groot/src/main/java/com/greenfox/groot/controller/GuardianController.java
+++ Week09/Day01/groot/src/main/java/com/greenfox/groot/controller/GuardianController.java
@@ -3,7 +3,6 @@ package com.greenfox.groot.controller;
 
 import com.greenfox.groot.model.ErrorMessage;
 import com.greenfox.groot.model.Message;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,9 +11,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GuardianController {
 
-  @Autowired
-  Message message;
-
   @ExceptionHandler(Exception.class)
   public ErrorMessage someException(Exception e) {
     return new ErrorMessage("I am Groot!");
@@ -22,8 +18,7 @@ public class GuardianController {
 
 @GetMapping(value = "/groot/{input}")
   public Message message(@PathVariable("input") String input){
-  message.setReceived(input);
-  message.setTranslated("I am groot!");
+  Message message = new Message();
   return message;
 }
 
--- Week09/Day01/groot/src/test/java/com/greenfox/groot/GuardianControllerTest.java
+++ Week09/Day01/groot/src/test/java/com/greenfox/groot/GuardianControllerTest.java
@@ -5,12 +5,18 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = GrootApplication.class)
@@ -29,7 +35,11 @@ public class GuardianControllerTest {
   }
 
   @Test
-  public void yourFirstTest(){
+  public void testStatusOk() throws Exception {
+    mockMvc.perform(get("/groot")
+            .contentType(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk())
+            .andExpect(content().contentType(contentType));
   }
 
 }
